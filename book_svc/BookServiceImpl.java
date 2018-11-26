d0ce0cec277e6a6632d34f0db1d48b161fcd457d
haifadhila <haifadhila.ilma@gmail.com>
2018-11-26 16:58:57 +0700

    move files

---
+++ book_svc/BookServiceImpl.java
@@ -0,0 +1,88 @@
+package com.journaldev.jaxws.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Set;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import javax.jws.WebService;
+
+import com.journaldev.jaxws.beans.Book;
+
+@WebService(endpointInterface = "com.journaldev.jaxws.service.BookService")
+public class BookServiceImpl implements BookService {
+
+	// GET BOOKS: TO RETURN BOOK RESULTS FROM SEARCHBOOK
+	@Override
+	public Book[] getBooks(String keyword) throws IOException, JSONException{
+		// Replace whitespace to "+" for keyword
+		keyword = keyword.replace(" ","+");
+
+		// Define User
+		String USER_AGENT = "Mozilla/5.0";
+		// Define Google Books API URL
+		String GET_URL = "https://www.googleapis.com/books/v1/volumes?q="+keyword;
+
+		// Get URL and check connection
+		URL obj = new URL(GET_URL);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		int responseCode = con.getResponseCode();
+		System.out.println("GET Response Code :: " + responseCode);
+
+		StringBuffer response = new StringBuffer();
+		// If response code = 200, then success and read response
+		if (responseCode == HttpURLConnection.HTTP_OK) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					con.getInputStream()));
+			String inputLine;
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			// Print result from Google Books API
+			//System.out.println(response.toString());
+		}
+		else { // if not success
+			System.out.println("GET request failed");
+		}
+
+		// Convert string to JSON
+		JSONObject booklist = new JSONObject(response.toString());
+
+    System.out.println(booklist.getString("kind"));
+
+		Book[] b = new Book[10];
+		return b;
+	}
+
+	// GET BOOK DETAIL: TO RETURN BOOK DETAILS TO SHOW
+	@Override
+	public Book getBookDetail(String idBook){
+		Book b = new Book();
+		return b;
+	}
+
+	// TO BUY BOOK
+	@Override
+	public boolean buyBook(String id) {
+		return true;
+	}
+
+	// RECOMMEND BOOK: TO RETURN RECOMMENDED BOOK BASED ON CATEGORY
+	@Override
+	public Book[] recommendBooks(String category){
+		Book[] b = new Book[10];
+		return b;
+	}
+
+}
