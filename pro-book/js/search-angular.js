0680f7adb17bdef8587e6a11a4f8ef5f05608b34
haifadhila <haifadhila.ilma@gmail.com>
2018-11-28 18:27:34 +0700

    angular for search result

---
+++ pro-book/js/search-angular.js
@@ -0,0 +1,29 @@
+// ANGULAR FOR SEARCH RESULTS
+
+angular.module('searchBook',[]).controller('SearchCon', function($scope){
+  var result = this;
+  console.log(result);
+  result.books = [];
+  result.keyword = "";
+
+  result.search = function(){
+    while (result.books.length > 0){
+      result.books.pop();
+    }
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        json = JSON.parse(this.responseText);
+        angular.forEach(json.item, function(book){
+          result.books.push(book);
+        });
+        $scope.$apply();
+        console.log(result.books);
+      }
+    };
+    console.log(result.keyword);
+    xhttp.open("POST", "search?search="+result.keyword, true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("keyword="+result.keyword);
+  }
+});
