aa0933ff976877bb55da44818f24039f65f5c716
haifadhila <haifadhila.ilma@gmail.com>
2018-11-30 10:49:30 +0700

    add ratings from db

--- pro-book/controllers/search.php
+++ pro-book/controllers/search.php
@@ -10,9 +10,35 @@ if (!isset($_REQUEST['search'])) {
 
     $wsdl = 'http://localhost:8888/ws/probook?wsdl';
 
-      $soap = new SoapClient($wsdl);
-      $data = $soap->getBooks($_REQUEST['search']);
-      $result = json_encode($data);
-      echo($result);
+    $soap = new SoapClient($wsdl);
+    $data = $soap->getBooks($_REQUEST['search']);
+
+    // DATABASE CONNECT (to input reviews)
+    global $db_conn;
+    $reviewstmt = $db_conn->prepare('select rating
+      from Reviews natural join Transactions natural join Users
+      where idBook = ?');
+
+    for ($i = 0; $i < sizeof($data->item); $i++) {
+        $bookid = $data->item[$i]->idBook;
+        $reviewstmt->execute([$bookid]);
+        $reviews = $reviewstmt->fetchAll();
+        $reviewCount = count($reviews) + $data->item[$i]->ratingCount;
+        if($reviewCount == 0) {
+          $avgrating = 0;
+        }
+        else {
+          $sumrating = $data->item[$i]->ratingCount * $data->item[$i]->rating;
+          foreach ($reviews as $review) {
+            $sumrating += floatval($review['rating']);
+          }
+          $avgrating = $sumrating / $reviewCount;
+        }
+          $data->item[$i]->rating = $avgrating;
+          $data->item[$i]->ratingCount = $reviewCount;
+    }
+
+    $result = json_encode($data);
+    echo($result);
 }
 ?>
