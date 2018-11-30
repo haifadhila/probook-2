941fb7ff98798c29739b220cd4938adb74016fc4
Sammy <13516103@std.stei.itb.ac.id>
2018-11-30 06:06:48 +0000

    Implement deposit and withdrawal on transaction model

--- bank-svc/src/model/transactions.js
+++ bank-svc/src/model/transactions.js
@@ -1,5 +1,9 @@
 const asyncDb = require('../db/async-db')
 
+const getBalanceQuery = 'select balance from accounts where id=?'
+const insertTxnQuery = 'insert into transactions ' +
+      '(acct, amount, type, remarks) values (?, ?, ?, ?)'
+
 let rowToResult = (row) => {
     return {
         txnid: row.id,
@@ -29,15 +33,75 @@ let byAccount = async (accId) =>
         [accId])
     ).map(rowToResult)
 
-//let makeWithdrawal = (req, callback) => {
-//
-//}
+let makeDeposit = async (req) => {
+    const txnType = 'deposit'
+    if (!Number.isInteger(req.amount) || !Number.isInteger(req.accno))
+        throw 'bad params'
+    if (req.amount <= 0 || req.type !== txnType)
+        throw 'bad params'
+    if (req.remarks === undefined)
+        req.remarks = null
+    return await asyncDb.withTransaction(async (conn) => {
+        let result = await asyncDb.query(
+            conn,
+            'update accounts set balance = balance + ? where id=?',
+            [req.amount, req.accno])
+        if (result.affectedRows < 1)
+            throw 'no account'
+        result = await asyncDb.query(conn, getBalanceQuery, [req.accno])
+        let finalBalance = result[0].balance
+        result = await asyncDb.query(
+            conn, insertTxnQuery,
+            [req.accno, req.amount, txnType, req.remarks])
+        let txnid = result.insertId
+        return {
+            txnid: txnid,
+            accno: req.accno,
+            type: txnType,
+            amount: req.amount,
+            balance: finalBalance
+        }
+    })
+}
+
+let makeWithdrawal = async (req) => {
+    const txnType = 'withdrawal'
+    if (!Number.isInteger(req.amount) || !Number.isInteger(req.accno))
+        throw 'bad params'
+    if (req.amount <= 0 || req.type !== txnType)
+        throw 'bad params'
+    if (req.remarks === undefined)
+        req.remarks = null
+    return await asyncDb.withTransaction(async (conn) => {
+        let result = await asyncDb.query(
+            conn,
+            'update accounts set balance = balance - ? where id=?',
+            [req.amount, req.accno])
+        if (result.affectedRows < 1)
+            throw 'no account'
+        result = await asyncDb.query(conn, getBalanceQuery, [req.accno])
+        let finalBalance = result[0].balance
+        if (finalBalance < 0)
+            throw 'not enough funds'
+        result = await asyncDb.query(
+            conn, insertTxnQuery,
+            [req.accno, req.amount, txnType, req.remarks])
+        let txnid = result.insertId
+        return {
+            txnid: txnid,
+            accno: req.accno,
+            type: txnType,
+            amount: req.amount,
+            balance: finalBalance
+        }
+    })
+}
 
 module.exports = {
     byId,
     byAccount,
-    //makeDeposit,
-    //makeWithdrawal,
+    makeDeposit,
+    makeWithdrawal,
     //makeCorrection,
     //doTransfer
 }
