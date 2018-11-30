a3d74818cf09e451eac2c0969af466748eaaf3d1
haifadhila <haifadhila.ilma@gmail.com>
2018-11-30 10:50:14 +0700

    handle 0 results

--- pro-book/js/search-angular.js
+++ pro-book/js/search-angular.js
@@ -11,6 +11,7 @@ angular.module('searchBook',[]).controller('SearchCon', function($scope){
       result.books.pop();
     }
     document.getElementById("loader").style.display = "block";
+    document.getElementById("test").style.display = "none";
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
@@ -21,6 +22,7 @@ angular.module('searchBook',[]).controller('SearchCon', function($scope){
         $scope.$apply();
         console.log(result.books);
         document.getElementById("loader").style.display = "none";
+        document.getElementById("test").style.display = "flex";
       }
 
     };
