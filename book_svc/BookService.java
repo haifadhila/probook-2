adf099cc1ef9d7d397acd692f5a2ae6d4ee092c4
haifadhila <haifadhila.ilma@gmail.com>
2018-11-30 17:15:06 +0700

    minor changes

--- book_svc/BookService.java
+++ book_svc/BookService.java
@@ -6,19 +6,20 @@ import javax.jws.soap.SOAPBinding;
 import java.io.IOException;
 
 import bookservice.Book;
+import org.json.JSONException;
 
 @WebService
 @SOAPBinding(style = SOAPBinding.Style.RPC)
 public interface BookService{
 
 	@WebMethod
-	public Book[] getBooks(String keyword) throws IOException;
+	public Book[] getBooks(String keyword) throws IOException, JSONException;
 
 	@WebMethod
 	public Book getBookDetail(String idBook) throws IOException;
 
 	@WebMethod
-	public boolean buyBook(String idBook, int qty, String senderNum);
+	public boolean buyBook(String idBook, int qty, String senderNum) throws IOException;
 
 	@WebMethod
 	public Book[] recommendBooks(String category);
--- pro-book/js/search-angular.js
+++ pro-book/js/search-angular.js
@@ -15,6 +15,8 @@ angular.module('searchBook',[]).controller('SearchCon', function($scope){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseText)
+        console.log(this.keyword)
         json = JSON.parse(this.responseText);
         angular.forEach(json.item, function(book){
           result.books.push(book);
@@ -27,6 +29,7 @@ angular.module('searchBook',[]).controller('SearchCon', function($scope){
 
     };
     console.log(result.keyword);
+
     xhttp.open("POST", "search?search="+result.keyword, true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send("keyword="+result.keyword);
